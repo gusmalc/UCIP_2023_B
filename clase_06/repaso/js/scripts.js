3763d0d46ca99018eeeaea454c7d3c3c97ced66e
Gustavo Malcotti <gustavo@malcotti.com>
2023-07-11 18:02:35 -0300

    1

---
+++ clase_06/repaso/js/scripts.js
@@ -0,0 +1,7 @@
+let btn1 = document.getElementById("btn1");
+let caja1 = document.getElementById("caja1");
+
+
+btn1.addEventListener("click", function() {
+   caja1.style.backgroundColor = "#222"
+});
